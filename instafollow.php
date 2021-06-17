eaac4ea079673f65ebe9333a671b1460e6be547c
sokot19 <77233790+sokot19@users.noreply.github.com>
2021-06-17 14:40:00 +0430

    Update instafollow.php

--- instafollow.php
+++ instafollow.php
@@ -1,7 +1,7 @@
 [
   [
     {
-      "image": "special/orderr.png",
+      "image": "http://haditest.parsaspace.com/special/orderr.png",
       "action": "2",
       "bazar": "com.samane.fola",
       "url": "https://cafebazaar.ir/app/com.samane.fola"
